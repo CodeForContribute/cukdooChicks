2e8c4b7f4f42ae5c1ddfce76aa0fdacb886eb29c
shamma077 <102911773+shamma077@users.noreply.github.com>
2022-12-19 10:06:33 +0530

    Home Page

--- cukdoo/src/app/app-routing.module.ts
+++ cukdoo/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BestdealComponent } from './bestdeal/bestdeal.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path:'home', component: HomeComponent },
+  { path:'menu', component: MenuComponent },
+  { path:'bestdeal', component:BestdealComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cukdoo/src/app/app.module.ts
+++ cukdoo/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { BestdealComponent } from './bestdeal/bestdeal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    MenuComponent,
+    BestdealComponent
   ],
   imports: [
     BrowserModule,
